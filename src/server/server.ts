aab5b6cf6a38d4c083b7ca9ddaa32d5294af4614
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-07-08 14:13:16 -0400

    updated

--- src/server/server.ts
+++ src/server/server.ts
@@ -25,11 +25,12 @@ const app = express();
 
 
 
-let p = path.join(__dirname, '../public');
+let p = path.join(__dirname, '/public');
 console.log(p);
 
 app.use(express.json())
 app.use(express.static(p));
+// app.use(express.static(path.join(__dirname,'public/'))
 app.use('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, `../public/index.html`));
 })
