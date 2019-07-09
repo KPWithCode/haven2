c2a873c9b1f44e5abc5eff038de02b42a014325e
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-07-09 15:55:34 -0400

    Final Change

--- src/server/server.ts
+++ src/server/server.ts
@@ -22,13 +22,13 @@ const app = express();
 //       });
 
 //   });
-let p = path.join(__dirname, '../../public');
+let p = path.join(__dirname, '../public');
 // console.log(p);
 app.use(express.json())
 app.use(express.static(p));
 app.use(apiRouter);
 app.use('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, `../../public/index.html`));
+    res.sendFile(path.join(__dirname, `../public/index.html`));
 })
 
 app.use(passport.initialize());
