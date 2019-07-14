2d59588752a5eb7ffddf5dafba3ce00e5ed4f8df
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-07-14 12:03:27 -0400

    Added pool

--- src/server/config/production.ts
+++ src/server/config/production.ts
@@ -18,8 +18,8 @@ export default {
             password: process.env.KNEX_PASS
         },
         pool: {
-            min: process.env.KNEX_MIN,
-            max: process.env.KNEX_MAX
+            min: process.env.KNEX_POOL_MIN,
+            max: process.env.KNEX_POOL_MAX
         }
     }
 }
